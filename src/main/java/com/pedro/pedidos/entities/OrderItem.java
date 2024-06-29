51a6684b6d2d45516cbfe6bed00c7ee8ea016283
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-06-29 14:55:52 -0300

    Implementando metodos subtotal e total

--- src/main/java/com/pedro/pedidos/entities/OrderItem.java
+++ src/main/java/com/pedro/pedidos/entities/OrderItem.java
@@ -62,6 +62,10 @@ public class OrderItem implements Serializable{
         this.price = price;
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -87,5 +91,4 @@ public class OrderItem implements Serializable{
         return true;
     }
 
-    
 }
