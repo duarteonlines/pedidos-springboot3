03a5e37bb8b6234a3e27bf8e33259abc68a8b230
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-06-30 11:02:48 -0300

    .

--- src/main/java/com/pedro/pedidos/repositories/OrderItemRepository.java
+++ src/main/java/com/pedro/pedidos/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.pedro.pedidos.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pedro.pedidos.entities.OrderItem;
+import com.pedro.pedidos.entities.pk.OrderItemPK;
 
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
--- src/main/java/com/pedro/pedidos/services/UserService.java
+++ src/main/java/com/pedro/pedidos/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.pedro.pedidos.entities.User;
 import com.pedro.pedidos.repositories.UserRepository;
+import com.pedro.pedidos.services.exceptions.DatabaseException;
 import com.pedro.pedidos.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -29,19 +34,29 @@ public class UserService {
         return repository.save(obj);
     }
 
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
-    }
-
-    public void delete(Long id) {
-        repository.deleteById(id);
     }
 }
