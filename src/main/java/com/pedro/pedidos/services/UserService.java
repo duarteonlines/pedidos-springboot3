9049f45c4f9a416fa5a23dd4ca877652905f3833
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-06-30 10:01:48 -0300

    Implementando Exceptions no busca por id

--- src/main/java/com/pedro/pedidos/services/UserService.java
+++ src/main/java/com/pedro/pedidos/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.pedro.pedidos.entities.User;
 import com.pedro.pedidos.repositories.UserRepository;
+import com.pedro.pedidos.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
